198ec6e685288047fd791fe18dbf7ba545bbeeb3
root <rick@rudelinux.org>
2019-09-30 14:14:52 +0100

    Finish update

--- update.sh
+++ update.sh
@@ -1,17 +1,18 @@
 #!/usr/bin/env bash
 
-function version_gt() { test "$(printf '%s\n' "$@" | sort -V | head -n 1)" != "$1"; }
+set -euf -o pipefail
 
-UPDATE_URL=https://github.com/rickr/metar_map.git
+GIT=$(which git)
+$GIT fetch -q
 
-LOCAL_VERSION=$(cat VERSION)
-REMOTE_VERSION=$(git ls-remote --tags | grep tags | awk -F '/'  '{ print $NF }')
+UPSTREAM=${1:-'@{u}'}
+LOCAL=$($GIT rev-parse @)
+REMOTE=$($GIT rev-parse "$UPSTREAM")
+BASE=$($GIT merge-base @ "$UPSTREAM")
 
-echo $LOCAL_VERSION
-echo $REMOTE_VERSION
-
-if version_gt $REMOTE_VERSION $LOCAL_VERSION; then
-  echo "Updating from $LOCAL_VERSION to $REMOTE_VERSION"
-  # something like git pull && git merge
+if [ $LOCAL = $REMOTE ]; then
+    echo "Up-to-date"
+elif [ $LOCAL = $BASE ]; then
+    echo "Update available"
+    $GIT pull
 fi
-
