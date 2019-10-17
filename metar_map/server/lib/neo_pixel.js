b5d2a6a4f9255d43b50a369e69ff25d7f17e46c2
root <rick@rudelinux.org>
2019-10-17 19:55:27 +0100

    update map

--- metar_map/server/lib/neo_pixel.js
+++ metar_map/server/lib/neo_pixel.js
@@ -2,23 +2,60 @@ let five = require("johnny-five");
 let pixel = require("node-pixel");
 let Raspi = require("raspi-io").RaspiIO;
 
-const board = new five.Board({ io: new Raspi() });
+const board = new five.Board({
+  io: new Raspi(),
+  repl: false
+});
 
-const config = require('./config')
-const MetarRequest = require('./metar_request')
-
-console.log(MetarRequest.as_json())
+const config = require('./config');
+const MetarRequest = require('./metar_request').MetarRequest;
 
 board.on("ready", function(){
   strip = new pixel.Strip({
-      color_order: pixel.COLOR_ORDER.GRB,
-      board: this,
-      controller: "I2CBACKPACK",
-      strips: [21]
+    color_order: pixel.COLOR_ORDER.GRB,
+    board: this,
+    controller: "I2CBACKPACK",
+    strips: [21]
   });
 
-  strip.on("ready", function(WeatherRequest){
-    console.log("Gogogo");
-    console.log(config.airports);
+  function updateMap(){
+    const metars = MetarRequest.as_json();
+
+    config.airports.forEach(function(airport, i){
+      let skyCondition = null;
+
+      if(metars.airports[i].flight_category){
+        skyCondition = metars.airports[i].flight_category._text
+      }
+
+      let ledColor = '';
+      switch(skyCondition){
+        case 'VFR':
+          ledColor = config.led.colors.vfr
+          break;
+        case 'MVFR':
+          ledColor = config.led.colors.mvfr
+          break;
+        case 'IFR':
+          ledColor = config.led.colors.ifr
+          break;
+        case 'LIFR':
+          ledColor = config.led.colors.lifr
+          break;
+      }
+
+      console.log("Updating " + airport + " to " + skyCondition + " (" + ledColor + ")");
+      if(ledColor){
+        strip.pixel(i).color(ledColor);
+      } else {
+        strip.pixel(i).off();
+      }
+    })
+    strip.show();
+    setTimeout(updateMap, 1000 * 10)
+  }
+
+  strip.on("ready", function(){
+    updateMap();
   });
 });
